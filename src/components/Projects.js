7b633d5852060cc8d508da7ac3a58a1272109b01
eldoraboo <eldoraboo.mby@gmail.com>
2023-02-26 21:12:36 +0800

    feat: add other projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,12 +10,14 @@ import {
   CardBody,
   Image,
   Heading,
-  CardFooter,
+  SimpleGrid,
   Badge,
+  Link,
 } from "@chakra-ui/react";
 import tps_site from "../assets/tps_site.png";
 import scales_io from "../assets/scales_io.png";
 import auto_labeling from "../assets/auto_labeling.png"
+import pixel_duck from "../assets/pixel_duck.png"
 import { Fade } from "react-reveal";
 
 const projects = [
@@ -24,7 +26,7 @@ const projects = [
     description:
       "Built a landing page for a construction firm to present them professionally. The first prototype was built in under 2 days.",
     badges: [
-      { text: "React.js", colorScheme: "blue" },
+      { text: "React.js", colorScheme: "cyan" },
       { text: "Chakra UI", colorScheme: "teal" },
     ],
     buttons: [
@@ -38,9 +40,10 @@ const projects = [
   },
   {
     name: "Scales.io",
-    description: "A piano key visualizer for scales.",
+    description:
+      "Developed a custom piano key visualizer for music scales, providing a user-friendly tool for musicians to practice and improve their skills. Utilized React.js and Chakra UI to create an interactive and visually appealing interface. The tool received positive feedback from users.",
     badges: [
-      { text: "React.js", colorScheme: "blue" },
+      { text: "React.js", colorScheme: "cyan" },
       { text: "Chakra UI", colorScheme: "teal" },
     ],
     buttons: [
@@ -55,7 +58,7 @@ const projects = [
   {
     name: "Auto-Labeling Pipeline",
     description:
-      "Auto Labeling Pipeline for Named Entity Recognition & Test Classification.",
+      "Built a highly efficient auto-labeling pipeline for text classification, utilizing cutting-edge technologies such as spaCy, Hugging Face, and Docker. The pipeline significantly reduced manual labeling efforts and improved the accuracy of text classification tasks. Demonstrated expertise in NLP and data engineering by seamlessly integrating various tools and technologies to create a cohesive solution.",
     badges: [
       { text: "Python", colorScheme: "yellow" },
       { text: "Docker", colorScheme: "black" },
@@ -68,6 +71,175 @@ const projects = [
       },
     ],
     image: auto_labeling,
+  },
+  {
+    name: "Pixel Duck",
+    description:
+      "Designed and programmed a web app that generates unique pixel ducks based on user-specified or randomized color inputs. Allows users to copy the generated duck in SVG format. Technologies used include HTML, CSS, React.js, and Chakra UI.",
+    badges: [
+      { text: "React.js", colorScheme: "cyan" },
+      { text: "Chakra UI", colorScheme: "teal" },
+    ],
+    buttons: [
+      { text: "Live Site", href: "https://pixel-duck.netlify.app/" },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/pixel-duck",
+      },
+    ],
+    image: pixel_duck,
+  },
+];
+
+const others = [
+  {
+    name: "PairALegal",
+    description:
+      "Chatbot paired with a website depository filled with succinct profiles of lawyers offering their services.",
+    badges: [
+      { text: "HTML", colorScheme: "red" },
+      { text: "CSS", colorScheme: "blue" },
+    ],
+    buttons: [
+      { text: "Live Site", href: "https://eldoraboo.github.io/PairALegal/" },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/PairALegal",
+      },
+    ],
+  },
+  {
+    name: "Trello-Postman Client",
+    description: "Trello API optimised as a collection for Postman.",
+    badges: [{ text: "Postman", colorScheme: "orange" }],
+    buttons: [
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/trello-postman",
+      },
+    ],
+  },
+  {
+    name: "Sencilla Theme",
+    description:
+      "Minimalistic, mobile-friendly one-page resume built with HTML, CSS, and JavaScript.",
+    badges: [
+      { text: "HTML", colorScheme: "red" },
+      { text: "CSS", colorScheme: "blue" },
+      { text: "JavaScript", colorScheme: "yellow" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://eldoraboo.github.io/sencilla-theme/",
+      },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/sencilla-theme",
+      },
+    ],
+  },
+  {
+    name: "Deploying Docker",
+    description: "A blog documenting how to deploy Docker containers.",
+    badges: [
+      { text: "Markdown", colorScheme: "purple" },
+      { text: "Docker", colorScheme: "blue" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://freight.gatsbyjs.io/",
+      },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/freight",
+      },
+    ],
+  },
+  {
+    name: "Personal Website V1",
+    description: "Version 1 of my personal website.",
+    badges: [
+      { text: "HTML", colorScheme: "red" },
+      { text: "CSS", colorScheme: "blue" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://eldoraboo.github.io/v1",
+      },
+    ],
+  },
+  {
+    name: "Personal Website V2",
+    description: "Version 2 of my personal website.",
+    badges: [
+      { text: "React.js", colorScheme: "cyan" },
+      { text: "GraphQL", colorScheme: "pink" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://eldoraboo-v2.netlify.app",
+      },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/eldora-gatsby-site",
+      },
+    ],
+  },
+  {
+    name: "Personal Website V3",
+    description: "Version 3 of my personal website. (This site!)",
+    badges: [
+      { text: "React.js", colorScheme: "cyan" },
+      { text: "Chakra UI", colorScheme: "teal" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://eldoraboo.netlify.app",
+      },
+      {
+        text: "GitHub Repo",
+        href: "https://github.com/eldoraboo/portfolio-v3",
+      },
+    ],
+  },
+  {
+    name: "Online Resume",
+    description: "My online resume based off Sencilla Theme.",
+    badges: [
+      { text: "HTML", colorScheme: "red" },
+      { text: "CSS", colorScheme: "blue" },
+    ],
+    buttons: [
+      {
+        text: "Live Site",
+        href: "https://eldoraboo.github.io/",
+      },
+    ],
+  },
+  {
+    name: "Metabase",
+    description: "Building a Business Intelligence System with BigQuery.",
+    badges: [{ text: "SQL", colorScheme: "gray" }],
+    buttons: [],
+  },
+  {
+    name: "Trello Workflow",
+    description:
+      "An automated workflow system to extract data from the Trello API using Python 3 & Postman.",
+    badges: [{ text: "Python", colorScheme: "yellow" }],
+    buttons: [],
+  },
+  {
+    name: "Auto Car Report",
+    description:
+      "An automated system built with Flask that generates excel spreadsheet reports.",
+    badges: [{ text: "Python", colorScheme: "yellow" }],
+    buttons: [],
   },
 ];
 
@@ -100,10 +272,7 @@ export default function Projects() {
                   }}
                   overflow="hidden"
                 >
-                  <Image
-                    objectFit="cover"
-                    src={project.image}
-                  />
+                  <Image objectFit="cover" src={project.image} />
 
                   <Stack>
                     <CardBody align="left">
@@ -118,10 +287,7 @@ export default function Projects() {
                           </a>
                         ))}
                       </HStack>
-                    </CardBody>
-
-                    <CardFooter>
-                      <HStack spacing={2}>
+                      <HStack pt={4} spacing={2}>
                         {project.badges.map((badge) => (
                           <Badge
                             key={badge.text}
@@ -131,12 +297,55 @@ export default function Projects() {
                           </Badge>
                         ))}
                       </HStack>
-                    </CardFooter>
+                    </CardBody>
                   </Stack>
                 </Card>
               </Fade>
             ))}
           </Stack>
+          <Text color={"gray.600"} fontSize={"xl"} px={4}>
+            Other Projects
+          </Text>
+          <SimpleGrid columns={[2, null, 3]} x={4} px={4} spacing={4}>
+            {others.map((other) => (
+              <Fade bottom>
+                <Card key={other.name} h="40vh">
+                  <Stack>
+                    <CardBody align="left">
+                      <Heading size="sm">{other.name}</Heading>
+
+                      <Text fontSize="sm" py={2}>
+                        {other.description}
+                      </Text>
+
+                      <HStack spacing={2}>
+                        {other.buttons.map((button) => (
+                          <Link
+                            key={button.text}
+                            href={button.href}
+                            color="pink.400"
+                          >
+                            {button.text}
+                          </Link>
+                        ))}
+                      </HStack>
+                      <HStack flexWrap="wrap" pt={4} spacing={2}>
+                        {other.badges.map((badge) => (
+                          <Badge
+                            my={2}
+                            key={badge.text}
+                            colorScheme={badge.colorScheme}
+                          >
+                            {badge.text}
+                          </Badge>
+                        ))}
+                      </HStack>
+                    </CardBody>
+                  </Stack>
+                </Card>
+              </Fade>
+            ))}
+          </SimpleGrid>
         </Stack>
       </Container>
     </>
