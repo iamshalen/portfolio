ffc1d11b43c92443bfe15a582e3ea157a14e4c55
eldoraboo <eldoraboo.mby@gmail.com>
2023-04-01 17:31:50 +0800

    feat: markdown parser

---
+++ public/content/Experience.md
@@ -0,0 +1,104 @@
+## Nika.eco
+- *Full-Stack Developer* | Aug'23 - Dec'23
+- ![nk](../assets/nk.jpeg)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+  - Pending Start Date [red]
+- List Items:
+
+## Moody's Analytics
+- *Software Engineer* | Jun'23 - Aug'23
+- ![ma](../assets/ma.jpeg)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+  - Pending Start Date [red]
+- List Items:
+
+## Beansprout
+- *Data Analytics* | Mar'23 - May'23
+- ![bs](../assets/bs.jpeg)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+  - In Progress [orange]
+- List Items:
+  - Performed association rule mining and text classification analysis to understand user path exploration.
+  - Worked with digital analytics software like Google Analytics and Amplitude to track site activity.
+
+## Urban Redevelopment Authority
+- *Data Engineer* | Jan'23 - Mar'23
+- ![ura](../assets/ura.png)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+- List Items:
+  - Piloted the integration of a optimization modeling tool (Python) into URA's existing systems with Docker and Node.js.
+  - Performed data analytics on movement of traffic and implemented NLP (Natural Language Processing) on entity categorization of feedback.
+  - Employed port forwarding to securely expose the necessary ports for communication between the front-end and the optimization model.
+
+## Lumens Auto
+- *Data Scientist* | Sep'22 - Dec'22
+- ![la](../assets/la.jpeg)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+- List Items:
+  - Utilized a range of APIs including Postman, Trello, and CarTrack to extract data and perform data analytics on datasets with up to 300K rows.
+  - Built a comprehensive business intelligence system on Metabase, providing insightful data visualisations for stakeholders.
+  - Implemented automated ETL workflows with Python and BigQuery SQL, saving up to 730 man-hours annually.
+
+## RIMM Sustainability
+- *Data Scientist* | May'22 - Jul'22
+- ![ri](../assets/ri.png)
+- Tags: Work
+- Badges:
+  - Internship [blue]
+- List Items:
+  - Designed and implemented a Python API for sequence labeling with spaCy models, resulting in improved accuracy and efficiency of NLP tasks.
+  - Leveraged open-source tools such as Doccano and HuggingFace to build an optimized BERT (Bidirectional Encoder Representations from Transformer) model.
+  - Came up with a production ready interface by the end of internship period and presented the product to the CTO directly.
+
+## Roboto Coding Academy
+- *Technology Educator* | Nov'20 - Present
+- ![ro](../assets/ro.png)
+- Tags: Teaching
+- Badges:
+  - Education [yellow]
+  - In Progress [orange]
+- List Items:
+  - Teach and conduct coding, STEM and technology related classes for up to a class of 30 students (including schools like Assumption English School).
+  - Oversee and train students in preparation of coding & robotics competitions (in collaboration with DSTA, DSO National Laboratories) that held at a national level.
+  - Provided support for students outside of curriculum hours for them to embark on personal projects like a landing page for their school's CCA (Xinmin Secondary).
+
+## Construcshare
+- *Front-End Developer* | Feb'23 - Present
+- ![cs](../assets/cs.jpeg)
+- Tags: Other
+- Badges:
+  - Project [green]
+  - In Progress [orange]
+- List Items:
+  - Developing an e-commerce simulation model for construction materials.
+  - Working on an interface to run, tweak and visualize the model.
+
+## Google Developer Student Clubs
+- *Software Engineer* | Sep'22 - Present
+- ![gg](../assets/gg.png)
+- Tags: Other
+- Badges:
+  - Project [green]
+  - In Progress [orange]
+- List Items:
+  - Designed and developed an intuitive admin portal with a user-friendly interface, providing an efficient way to manage course materials and users.
+  - Adopted front-end frameworks such as React.js and Chakra UI to create a highly responsive and engaging user interface.
+  - Engineered a full-stack API with Node.js and GraphQL, providing robust and scalable solutions for delivering online education.
+
+## NUS Statistics & Data Science Society
+- *Vice-Director of Marketing* | Dec'21 - May'22
+- ![sd](../assets/sd.jpeg)
+- Tags: Other
+- Badges:
+  - Project [green]
+- List Items:
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -22,9 +22,9 @@ import {
 import ma from "../assets/ma.jpeg";
 import bs from "../assets/bs.jpeg";
 import ura from "../assets/ura.png";
-import gg from "../assets/gg.png"
+import gg from "../assets/gg.png";
 import la from "../assets/la.jpeg";
-import ri from "../assets/ri.png"
+import ri from "../assets/ri.png";
 import ro from "../assets/ro.png";
 import sd from "../assets/sd.jpeg";
 import cs from "../assets/cs.jpeg";
@@ -32,145 +32,11 @@ import nk from "../assets/nk.jpeg";
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Fade } from "react-reveal";
 import { useState } from "react";
-
-const experience = [
-  {
-    image: nk,
-    company: "Nika.eco",
-    position: "Full-Stack Developer",
-    duration: "Aug'23 - Dec'23",
-    badges: [
-      { name: "Internship", colorScheme: "blue" },
-      { name: "Pending Start Date", colorScheme: "red" },
-    ],
-    listItems: [],
-    tags: "Work",
-  },
-  {
-    image: ma,
-    company: "Moody's Analytics",
-    position: "Software Engineer",
-    duration: "Jun'23 - Aug'23",
-    badges: [
-      { name: "Internship", colorScheme: "blue" },
-      { name: "Pending Start Date", colorScheme: "red" },
-    ],
-    listItems: [],
-    tags: "Work",
-  },
-  {
-    image: bs,
-    company: "Beansprout",
-    position: "Data Analytics",
-    duration: "Mar'23 - May'23",
-    badges: [
-      { name: "Internship", colorScheme: "blue" },
-      { name: "In Progress", colorScheme: "orange" },
-    ],
-    listItems: [
-      "Performed association rule mining and text classification analysis to understand user path exploration.",
-      "Worked with digital analytics software like Google Analytics and Amplitude to track site activity."
-    ],
-    tags: "Work",
-  },
-  {
-    image: ura,
-    company: "Urban Redevelopment Authority",
-    position: "Data Engineer",
-    duration: "Jan'23 - Mar'23",
-    badges: [{ name: "Internship", colorScheme: "blue" }],
-    listItems: [
-      "Piloted the integration of a optimization modeling tool (Python) into URA's existing systems with Docker and Node.js.",
-      "Performed data analytics on movement of traffic and implemented NLP (Natural Language Processing) on entity categorization of feedback.",
-      "Employed port forwarding to securely expose the necessary ports for communication between the front-end and the optimization model.",
-    ],
-    tags: "Work",
-  },
-  {
-    image: la,
-    company: "Lumens Auto",
-    position: "Data Scientist",
-    duration: "Sep'22 - Dec'22",
-    badges: [{ name: "Internship", colorScheme: "blue" }],
-    listItems: [
-      "Utilized a range of APIs including Postman, Trello, and CarTrack to extract data and perform data analytics on datasets with up to 300K rows.",
-      "Built a comprehensive business intelligence system on Metabase, providing insightful data visualisations for stakeholders.",
-      "Implemented automated ETL workflows with Python and BigQuery SQL, saving up to 730 man-hours annually.",
-    ],
-    tags: "Work",
-  },
-  {
-    image: ri,
-    company: "RIMM Sustainability",
-    position: "Data Scientist",
-    duration: "May'22 - Jul'22",
-    badges: [{ name: "Internship", colorScheme: "blue" }],
-    listItems: [
-      "Designed and implemented a Python API for sequence labeling with spaCy models, resulting in improved accuracy and efficiency of NLP tasks.",
-      "Leveraged open-source tools such as Doccano and HuggingFace to build an optimized BERT (Bidirectional Encoder Representations from Transformer) model.",
-      "Came up with a production ready interface by the end of internship period and presented the product to the CTO directly.",
-    ],
-    tags: "Work",
-  },
-  {
-    image: ro,
-    company: "Roboto Coding Academy",
-    position: "Technology Educator",
-    duration: "Nov'20 - Present",
-    badges: [
-      { name: "Education", colorScheme: "yellow" },
-      { name: "In Progress", colorScheme: "orange" },
-    ],
-    listItems: [
-      "Teach and conduct coding, STEM and technology related classes for up to a class of 30 students (including schools like Assumption English School).",
-      "Oversee and train students in preparation of coding & robotics competitions (in collaboration with DSTA, DSO National Laboratories) that held at a national level.",
-      "Provided support for students outside of curriculum hours for them to embark on personal projects like a landing page for their school's CCA (Xinmin Secondary).",
-    ],
-    tags: "Teaching",
-  },
-  {
-    image: cs,
-    company: "Construcshare",
-    position: "Front-End Developer",
-    duration: "Feb'23 - Present",
-    badges: [
-      { name: "Project", colorScheme: "green" },
-      { name: "In Progress", colorScheme: "orange" },
-    ],
-    listItems: [
-      "Developing an e-commerce simulation model for construction materials.",
-      "Working on an interface to run, tweak and visualize the model.",
-    ],
-    tags: "Other",
-  },
-  {
-    image: gg,
-    company: "Google Developer Student Clubs",
-    position: "Software Engineer",
-    duration: "Sep'22 - Present",
-    badges: [
-      { name: "Project", colorScheme: "green" },
-      { name: "In Progress", colorScheme: "orange" },
-    ],
-    listItems: [
-      "Designed and developed an intuitive admin portal with a user-friendly interface, providing an efficient way to manage course materials and users.",
-      "Adopted front-end frameworks such as React.js and Chakra UI to create a highly responsive and engaging user interface.",
-      "Engineered a full-stack API with Node.js and GraphQL, providing robust and scalable solutions for delivering online education.",
-    ],
-    tags: "Other",
-  },
-  {
-    image: sd,
-    company: "NUS Statistics & Data Science Society",
-    position: "Vice-Director of Marketing",
-    duration: "Dec'21 - May'22",
-    badges: [{ name: "Project", colorScheme: "green" }],
-    listItems: [],
-    tags: "Other",
-  },
-];
+import ExperienceArray from "./ExperienceArray";
 
 export default function Experience() {
+  const experience = ExperienceArray();
+
   const [selected, setSelected] = useState("Work");
 
   const handleSelected = (value) => {
