e5b9e4291c40a105a7d96ab95ef275fcbefa4732
eldoraboo <eldoraboo.mby@gmail.com>
2023-04-22 05:07:19 +0800

    img: change pixel duck

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
-import Nav from './components/NavBar';
-import Header from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Nav from "./components/NavBar";
+import Header from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  // Available Colours:
-  // blue, cyan, gray, green, orange, pink, purple, red, teal, yellow
+  const [color, setColor] = useState("");
 
-  // edit this variable to change the color theme
-  const color = "pink";
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:1337/api/profile");
+      const data = await response.json();
+      setColor(data.data.attributes.themeColour);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
