ba47952238db6af1fd46fe9ec7697faffa1f1eca
eldoraboo <eldoraboo.mby@gmail.com>
2023-04-28 12:56:44 +0800

    feat: add spinner

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { Spinner, Flex } from "@chakra-ui/react";
 
 function App() {
   const [color, setColor] = useState("");
+  const [spinner, setSpinner] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -18,19 +20,35 @@ function App() {
       );
       const data = await response.json();
       setColor(data.data.attributes.themeColour);
+      setTimeout(() => setSpinner(false), 1000); // set spinner to false after 10 seconds
     }
     fetchData();
   }, []);
 
   return (
     <>
-      <Nav color={color} />
-      <Header color={color} />
-      <About color={color} />
-      <Experience color={color} />
-      <Projects color={color} />
-      <Contact color={color} />
-      <Footer />
+      {spinner ? ( // display spinner if spinner state is true
+        <Flex align="center" justify="center" height="100vh">
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color={`${color}.500`}
+            size="xl"
+          />
+        </Flex>
+      ) : (
+        // display content if spinner state is false
+        <>
+          <Nav color={color} />
+          <Header color={color} />
+          <About color={color} />
+          <Experience color={color} />
+          <Projects color={color} />
+          <Contact color={color} />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
